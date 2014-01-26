76a170ce24b059877e684b43383fbd1fba6d65ee
Ben Palmer <ben@pwscf.localdomain>
2014-01-26 23:14:04 +0000

    commit

---
+++ make.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gfortran -o activity.x src/kinds.f90 src/constants.f90 src/stringfunctions.f90 src/lsq.f90 src/maths.f90 src/initialise.f90 src/input.f90 src/prep.f90 src/productionLoss.f90 src/output.f90 src/activity.f90 
+sleep 1 
+rm *.mod 
